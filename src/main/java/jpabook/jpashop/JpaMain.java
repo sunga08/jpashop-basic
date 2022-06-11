c7da99855108952031c9f7f1b782ec3e4f97f74d
sunga <eert2260@naver.com>
2022-06-11 18:30:40 +0900

    요구사항 분석과 기본 매핑

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Order order = em.find(Order.class, 1L);
+            //order를 주문한 member를 찾고싶음
+            Long memberId = order.getMemberId();
+
+            Member member = em.find(Member.class, memberId);
+
+            Member findMember = order.getMember();
+
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String name;
+    private String street;
+    private String zipcode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
